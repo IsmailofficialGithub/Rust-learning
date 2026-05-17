2bb6f4bb6b4a7acf39777b91c401bae2f1862554
Ismail abbasi <ismail.official295@gmail.com>
2026-05-17 19:46:22 +0500

    remove unused string length function and related code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-fn main(){
-    println!("* Function that returns lenght of strings");
-    let string=String::from("Ismail");
-    let len=get_string_length(string);
-    println!("Solution : Length of string is {}",len);
-}
 
-fn get_string_length(str :String)->usize{
-    str.chars().count()
-}   
+
+fn main(){
+
+println!("Hello , wellcome to rust learing with me");
+}
