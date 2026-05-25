d5bca8e6faeebc95c6acd000152d5f4e8b55f0e6
Ismail abbasi <ismail.official295@gmail.com>
2026-05-25 11:16:44 +0500

    Tuple structs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,7 @@
-struct User {
-    name: String,
-    age: u8,
-    email: String,
-    active: bool,
-}
-fn main() {
-    let user1 = User {
-        name: String::from("Ismail"),
-        age: 22,
-        email: String::from("old@gmail.com"),
-        active: true,
-    };
-
-    let user2=User{
-        name:String::from("Rohan"),
-        ..user1
-    };
-
-    println!("{}",user1.name);
-    println!("{}",user2.name);
+fn main (){
+    let black = Color(0,0,0);
+    println!("{},{},{}",black.0,black.1,black.2);
 
 }
+
+struct Color(i32,i32,i32);
--- src/solutions/structs.rs
+++ src/solutions/structs.rs
@@ -73,3 +73,13 @@
 //     println!("{}",user2.name);
 
 // }
+
+
+            // tuple structs
+// fn main (){
+//     let black = Color(0,0,0);
+//     println!("{},{},{}",black.0,black.1,black.2);
+
+// }
+
+// struct Color(i32,i32,i32);
