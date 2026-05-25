068d74e8f58b4ff489922f0244bf2d5502aaaadb
Ismail abbasi <ismail.official295@gmail.com>
2026-05-25 10:54:44 +0500

    Structs in rust Syntax

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 
+//syntax
+struct User {
+    name : String,
+    age : u8,
+    email:String
+}
 
 fn main(){
+println!("Hello , This is about structs in rust");
 
-println!("Hello , wellcome to rust learing with me");
+let new_user= User{
+        name:String::from("Ismail Abbasi"),
+        age:19,
+        email:String::from("ismail.official295@gmail.com")
+};
+println!("{}",new_user.name);
+println!("{}",new_user.age);
+println!("{}",new_user.email);
+
+
+
+
 }
