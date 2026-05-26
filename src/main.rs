bb82bbf4353885e28f6debfaafe102a95966fe8f
Ismail abbasi <ismail.official295@gmail.com>
2026-05-26 06:35:52 +0500

    impt functions to struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
-fn main (){
-    let black = Color(0,0,0);
-    println!("{},{},{}",black.0,black.1,black.2);
-
+struct Rect {
+    width:u32,
+    height:u32
 }
 
-struct Color(i32,i32,i32);
+impl Rect{
+    fn area(&self) -> u32{
+        self.width*self.height
+    }
+    fn parameter (&self)-> u32{
+        2 * (self.width*self.height)
+    }
+}
+
+fn main(){
+    let rect1=Rect{
+        width:30,
+        height:40
+    };
+    let area=rect1.area();
+    let parameter=rect1.parameter();
+    println!("{},{}",rect1.width,rect1.height);
+    println!("{}",area);
+    println!("{}",parameter);
+}
--- src/solutions/structs.rs
+++ src/solutions/structs.rs
@@ -83,3 +83,31 @@
 // }
 
 // struct Color(i32,i32,i32);
+
+
+//             impl Functions to struct
+// struct Rect {
+//     width:u32,
+//     height:u32
+// }
+
+// impl Rect{
+//     fn area(&self) -> u32{
+//         self.width*self.height
+//     }
+//     fn parameter (&self)-> u32{
+//         2 * (self.width*self.height)
+//     }
+// }
+
+// fn main(){
+//     let rect1=Rect{
+//         width:30,
+//         height:40
+//     };
+//     let area=rect1.area();
+//     let parameter=rect1.parameter();
+//     println!("{},{}",rect1.width,rect1.height);
+//     println!("{}",area);
+//     println!("{}",parameter);
+// }
