78aa93f1c833f54a94e763123859c82c330e78ff
ismail abbasi <ismail.official295@gmail.com>
2026-06-24 17:31:23 +0500

    Refactor main function to demonstrate mutable variables; add new MutableVariables.rs example

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use chrono::{Local,Utc};
-
-fn main (){
-    let now =Utc::now();
-    println!("Current time is {}",now);
-
-    let formatted = now.format("%Y-%m-%d %H:%M:%S").to_string();
-    println!("Formatted time is {}",formatted);
-
-    let get_local_time = Local::now();
-    println!("Local time is {}",get_local_time);
+fn main(){
+    // keyword mut to make varible mutable
+     let mut vari:i32 = 34;
+    vari=100;
+    println!("{}",vari)
 }
-
---
+++ src/solutions/MutableVariables.rs
@@ -0,0 +1,6 @@
+fn main(){
+    // keyword mut to make varible mutable
+     let mut vari:i32 = 34;
+    vari=100;
+    println!("{}",vari)
+}
