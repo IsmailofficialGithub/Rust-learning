337072470ffca35084f174a4a5fbd667dace45c2
ismail abbasi <ismail.official295@gmail.com>
2026-06-25 13:28:58 +0500

    Refactor borrowing examples to consolidate mutable borrowing demonstration in main function

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-//Borrowing in rust
-
-//& is used to Borrow a variable
-fn create_string (){
-    let s1 =String::from("this is s1");
-    print_string(&s1);
-    println!("{}",s1); 
-
-}
-
-fn print_string(s2:&String){
-    println!("{}",s2)
-}
-
-
+// &mut mutable borrowing
 fn main(){
+    let mut s1 = String::from("Ismail");
+    some_function(&mut s1);
+    println!("{}",s1);  
+}
+
+fn some_function (s2:&mut String){
+    s2.push_str("Abbasi");
+    println!("{}",s2);
     
 }
--- src/solutions/borrowing.rs
+++ src/solutions/borrowing.rs
@@ -12,7 +12,16 @@ fn print_string(s2:&String){
     println!("{}",s2)
 }
 
+// &mut mutable borrowing
 
 fn main(){
+    let mut s1 = String::from("Ismail");
+    some_function(&mut s1);
+    println!("{}",s1);  
+}
+
+fn some_function (s2:&mut String){
+    s2.push_str("Abbasi");
+    println!("{}",s2);
     
 }
