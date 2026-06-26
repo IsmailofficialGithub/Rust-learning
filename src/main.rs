2d639d5531c5a8eef73f591350e2949e52f7420e
ismail abbasi <ismail.official295@gmail.com>
2026-06-26 16:52:29 +0500

    Refactor hashmap examples for clarity; update main function and enhance group_values_by_keys implementation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 //Hashmap in rust
 use std::collections::HashMap;
 
+fn main() {
+    let mut users: HashMap<String, u32> = HashMap::new();
 
-fn main(){
-    let mut users:HashMap<String,u32>=HashMap::new();
-    
-    users.insert(String::from("hashmap"),19);
-    users.insert(String::from("Data"),10);
+    users.insert(String::from("hashmap"), 19);
+    users.insert(String::from("Data"), 10);
 
-
-    let first_user=users.get("hashmap");
-    match first_user{
-        Some(value)=>println!("{:?}",value),
-        None => println!("No HASHMAP found")
+    let first_user = users.get("hashmap");
+    match first_user {
+        Some(value) => println!("{:?}", value),
+        None => println!("No HASHMAP found"),
     }
+    let input=vec![(String::from("class"),20)];
+    let my_hash_map=group_values_by_keys(input);
+    println!("{:?}",my_hash_map);
+
+}
+
+
+// Function that take Vector of tuple and return HashMap for that Tuple values
+//Dummy Vector of tuple => vec![(String::from"Str1",20),()]
+
+fn group_values_by_keys(pairs:Vec<(String,i32)>)-> HashMap<String,i32>{
+    let mut hm:HashMap<String,i32>=HashMap::new();
+    for (index,value) in pairs{
+        hm.insert(String::from(index),value);
+    }
+    return hm;
 }
--- src/solutions/collections/hashmaps.rs
+++ src/solutions/collections/hashmaps.rs
@@ -8,8 +8,27 @@ fn main() {
     users.insert(String::from("Data"), 10);
 
     let first_user = users.get("hashmap");
-    match first_user {
-        Some(value) => println!("{:?}", value),
-        None => println!("No HASHMAP found"),
+    // match first_user {
+    //     Some(value) => println!("{:?}", value),
+    //     None => println!("No HASHMAP found"),
+    // }
+
+    let my_hash_map=group_values_by_keys(vec![("class",20)]);
+    match my_hash_map{
+        Some(hash_map)=> println!(hash_map),
+        None => println!("Some thing wents wrong")
     }
+
+}
+
+
+// Function that take Vector of tuple and return HashMap for that Tuple values
+//Dummy Vector of tuple => vec![(String::from"Str1",20),()]
+
+fn group_values_by_keys(pairs:Vec<(String,i32)>)-> HashMap<String,i32>{
+    let mut hm:HashMap<String,i32>=HashMap::new();
+    for i in pairs{
+        hm.insert(String::from(i[0]),i[1]);
+    }
+    return hm;
 }
