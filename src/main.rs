6035859fd0ee7c00ca2d9ae44c6bdecb8e2fab18
ismail abbasi <ismail.official295@gmail.com>
2026-06-26 18:09:32 +0500

    Refactor main function in iterators.rs to demonstrate iterator usage with next() method; update main function in main.rs for consistency

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 
 fn main (){
-   let mut v1=vec![1,2,3,4,5];
+   let  v1=vec![1,2,3,4,5];
+    let mut v1_iter =v1.iter();
+
+    while let Some (val)=v1_iter.next(){
+        println!("{}",val);
+    }
    
-   let iter=v1.iter_mut();
-   for i in iter{
-    *i =*i+1;
-   }
-   println!("{:?}",v1);
 }
--- src/solutions/collections/iterators.rs
+++ src/solutions/collections/iterators.rs
@@ -36,3 +36,16 @@
 //    }
 //    println!("{:?}",v1);
 // }
+
+        //   Iterators using next() with while loop
+
+        
+fn main (){
+   let  v1=vec![1,2,3,4,5];
+    let mut v1_iter =v1.iter();
+
+    while let Some (val)=v1_iter.next(){
+        println!("{}",val);
+    }
+   
+}
