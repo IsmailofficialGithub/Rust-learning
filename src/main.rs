533e9a18ece835b79dabb307f9f078ac62ceae80
ismail abbasi <ismail.official295@gmail.com>
2026-06-26 20:31:20 +0500

    Remove first_word function and update main function to print a static word

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
-fn main() {
-    let name = String::from("Ismail Abbasi");
-
-    let first= first_word(&name);
-    println!("First Word of '{}' is {}",&name,&first);
-}
-
-fn first_word(str: &String) -> &str {
-    let mut index: usize = 0;
-    for i in str.chars() {
-        if i == ' ' {
-            break;
-        }
-        index = index + 1;
-    }
-    return &str[0..index];
+fn main (){
+    let word ="Isalfak";
+    println!("{}",word)
 }
