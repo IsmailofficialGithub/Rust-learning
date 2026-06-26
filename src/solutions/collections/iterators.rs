5ad73c9b3fbf56656e46662cd4a78225f4872884
ismail abbasi <ismail.official295@gmail.com>
2026-06-26 18:12:46 +0500

    Comment out main function in iterators.rs to prevent execution during testing

--- src/solutions/collections/iterators.rs
+++ src/solutions/collections/iterators.rs
@@ -40,12 +40,12 @@
         //   Iterators using next() with while loop
 
         
-fn main (){
-   let  v1=vec![1,2,3,4,5];
-    let mut v1_iter =v1.iter();
+// fn main (){
+//    let  v1=vec![1,2,3,4,5];
+//     let mut v1_iter =v1.iter();
 
-    while let Some (val)=v1_iter.next(){
-        println!("{}",val);
-    }
+//     while let Some (val)=v1_iter.next(){
+//         println!("{}",val);
+//     }
    
-}
+// }
