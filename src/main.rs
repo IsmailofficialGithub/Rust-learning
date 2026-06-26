926dec2ce1bd132097d4fa2964b5446e9d6045c8
ismail abbasi <ismail.official295@gmail.com>
2026-06-26 19:39:18 +0500

    Add implementation of first_word function and example in Strings_vs_slices.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+fn main() {
+    let name = String::from("Ismail Abbasi");
 
-fn main (){
-   let  v1=vec![1,2,3,4,5];
-   
-    let iter= v1.iter().filter(|x|*x%2==1).map(|x|x+1);
-    let v2:Vec<i32> =iter.collect();
-    println!("{:?}",v2);
-   
+    let first= first_word(&name);
+    println!("First Word of '{}' is {}",&name,&first);
+}
+
+fn first_word(str: &String) -> &str {
+    let mut index: usize = 0;
+    for i in str.chars() {
+        if i == ' ' {
+            break;
+        }
+        index = index + 1;
+    }
+    return &str[0..index];
 }
