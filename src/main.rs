3fc7f825609e390c659ceb88e55e8af8c8ca3c4b
ismail abbasi <ismail.official295@gmail.com>
2026-06-26 17:08:52 +0500

    Remove HashMap implementation from main function; add iterator example in new iterators.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,8 @@
-//Hashmap in rust
-use std::collections::HashMap;
 
-fn main() {
-    let mut users: HashMap<String, u32> = HashMap::new();
 
-    users.insert(String::from("hashmap"), 19);
-    users.insert(String::from("Data"), 10);
-
-    let first_user = users.get("hashmap");
-    match first_user {
-        Some(value) => println!("{:?}", value),
-        None => println!("No HASHMAP found"),
+fn main (){
+    let nums = vec![2,2,3,4,5,35,34];
+    for num in nums {
+        println!("{}",num);
     }
-    let input=vec![(String::from("class"),20)];
-    let my_hash_map=group_values_by_keys(input);
-    println!("{:?}",my_hash_map);
-
-}
-
-
-// Function that take Vector of tuple and return HashMap for that Tuple values
-//Dummy Vector of tuple => vec![(String::from"Str1",20),()]
-
-fn group_values_by_keys(pairs:Vec<(String,i32)>)-> HashMap<String,i32>{
-    let mut hm:HashMap<String,i32>=HashMap::new();
-    for (index,value) in pairs{
-        hm.insert(String::from(index),value);
-    }
-    return hm;
 }
---
+++ src/solutions/collections/iterators.rs
@@ -0,0 +1,13 @@
+// Iterators
+
+            //Interating using for loop
+
+// fn main (){
+//     let nums = vec![2,2,3,4,5,35,34];
+//     for num in nums {
+//         println!("{}",num);
+//     }
+// }
+
+
+            
