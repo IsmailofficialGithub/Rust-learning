12407c6e9e38f9825da622b62912b7d77c72723c
ismail abbasi <ismail.official295@gmail.com>
2026-06-26 17:11:03 +0500

    Refactor main function to use iterator for improved performance; update iterators.rs with commented example

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@
 
 fn main (){
     let nums = vec![2,2,3,4,5,35,34];
-    for num in nums {
-        println!("{}",num);
+    let iter = nums.iter();
+    for value in iter {
+        println!("{}",value)
     }
+
 }
--- src/solutions/collections/iterators.rs
+++ src/solutions/collections/iterators.rs
@@ -10,4 +10,15 @@
 // }
 
 
+            //Iterating after creating and "Iterator"
+
             
+
+// fn main (){
+//     let nums = vec![2,2,3,4,5,35,34];
+//     let iter = nums.iter();
+//     for value in iter {
+//         println!("{}",value)
+//     }
+
+// }
