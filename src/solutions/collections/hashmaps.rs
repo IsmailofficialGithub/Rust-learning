40c899293ce62a3e1cbc2e8b6c66b35771db92a1
ismail abbasi <ismail.official295@gmail.com>
2026-06-26 14:48:56 +0500

    Refactor main function for improved readability; adjust formatting and spacing in hashmap example

--- src/solutions/collections/hashmaps.rs
+++ src/solutions/collections/hashmaps.rs
@@ -1,17 +1,15 @@
 //Hashmap in rust
 use std::collections::HashMap;
 
+fn main() {
+    let mut users: HashMap<String, u32> = HashMap::new();
 
-fn main(){
-    let mut users:HashMap<String,u32>=HashMap::new();
-    
-    users.insert(String::from("hashmap"),19);
-    users.insert(String::from("Data"),10);
+    users.insert(String::from("hashmap"), 19);
+    users.insert(String::from("Data"), 10);
 
-
-    let first_user=users.get("hashmap");
-    match first_user{
-        Some(value)=>println!("{:?}",value),
-        None => println!("No HASHMAP found")
+    let first_user = users.get("hashmap");
+    match first_user {
+        Some(value) => println!("{:?}", value),
+        None => println!("No HASHMAP found"),
     }
 }
