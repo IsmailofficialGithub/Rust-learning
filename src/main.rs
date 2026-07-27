f7b29d27a4f29f9e7bf8fbceb15ab5feaf0741f8
IsmailofficialGithub <ismail.official295@gmail.com>
2026-07-27 22:22:35 +0500

    Add Tokio dependency and refactor main function for async execution

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
+#[tokio::main (flavor="multi_thread" , worker_threads=10)]
 
-use std::{sync::mpsc, thread::{ spawn}};
-fn main ( ){
-let (tx,rx)= mpsc::channel();
+async fn main() {
+    // println!("Hello, world!");
+    print_with_wait().await;
+}
 
-    for i in 0..10 {
-        let producer=tx.clone();
-        spawn(move||{
-            let mut sum:i64 =0;
-            for j in i*10000000..(i+1*10000000)-1{
-                sum=sum+j;
-            }
-        producer.send(sum).unwrap();
-        });
-    }
-        drop(tx);
-    let mut final_sum=0;
-    for val in rx{
-        println!("recv value from thread");
-        final_sum= val+final_sum;
-    }
-    println!("{}",final_sum);
+async fn print_with_wait (){
+
+    std::thread::sleep(std::time::Duration::from_millis(5000));
+    print!("Print with wait .... waiting 5 seconds");
 }
